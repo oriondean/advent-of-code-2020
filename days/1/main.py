01ed7e7c9510aa7c085370346dea27ae37fda644
Dean Kerr <dean_k@live.co.uk>
2020-12-02 21:48:57 +0000

    Add day one implementation

---
+++ days/1/main.py
@@ -0,0 +1,29 @@
+from typing import List
+import functools
+
+
+def find_sum_of_two(list: list[int], sum: int):
+    for itemA in list:
+        for itemB in list:
+            if itemA + itemB == sum:
+                return [itemA, itemB]
+
+
+def find_sum_of_three(list: list[int], sum: int):
+    for itemA in list:
+        for itemB in list:
+            for itemC in list:
+                if itemA + itemB + itemC == sum:
+                    return [itemA, itemB, itemC]
+
+if __name__ == '__main__':
+    input: list[str] = open('input.txt').read().split()
+    expenses: list[int] = list(map(int, input))
+
+    resultOne: [int, int] = find_sum_of_two(expenses, 2020)
+    print('Part one', functools.reduce(lambda a, b: a*b, resultOne))
+
+    resultTwo: [int, int, int] = find_sum_of_three(expenses, 2020)
+    print('Part two', functools.reduce(lambda a, b: a*b, resultTwo))
+
+
