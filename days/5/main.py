abc1a0c036db9389951686f5f612cde1610d09aa
Dean Kerr <dean_k@live.co.uk>
2020-12-05 09:25:22 +0000

    Tidy day five implementation

--- days/5/main.py
+++ days/5/main.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
             row_range: [int, int] = [0, 127]
             column_range: [int, int] = [0, 7]
             for char in line:
-                if char == 'F' or 'B':
+                if char == 'F' or char == 'B':
                     delta = math.ceil((row_range[1] - row_range[0]) / 2) * (-1 if char == 'F' else 1)
                     row_range[1 if char == 'F' else 0] += delta
-                if char == 'L' or 'R':
-                    delta = math.ceil((column_range[1] - column_range[0]) / 2) * (-1 if char == 'L' else 1)
-                    column_range[1 if char == 'L' else 0] += delta
+                if char == 'L' or char == 'R':
+                    xdelta = math.ceil((column_range[1] - column_range[0]) / 2) * (-1 if char == 'L' else 1)
+                    column_range[1 if char == 'L' else 0] += xdelta
 
             seat_id = row_range[0] * 8 + column_range[0]
             seat_map[row_range[0]][column_range[0]] = seat_id
@@ -25,10 +25,10 @@ if __name__ == '__main__':
 
     file.close()
 
-    seat_list = list(chain.from_iterable(seat_map))[50:-50]  # chop off potentially non-existing seats
+    seat_list = list(chain.from_iterable(seat_map))[50:-50]
     index = 0
     while seat_list[index] == (seat_list[index + 1] - 1):
         index += 1
 
     print('Part one', highest_seat_id)
-    print('Part two', seat_list[index + 1])
+    print('Part two', seat_list[index] + 1)
